0c3192e732e127185a877c69e75bfc5d3179ec68
Guust Flater <guusflater@gmail.com>
2022-11-14 22:15:02 +0100

    buttons toegevoeg textboxs toegevoegd listbox background color button click events

--- BlackJackWPF/BlackJackWPF/MainWindow.xaml.cs
+++ BlackJackWPF/BlackJackWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,42 @@ namespace BlackJackWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int WillekeurigGetal;
+        private Random random;
+        private int Aantalpogingen;
+
         public MainWindow()
         {
             InitializeComponent();
         }
-    }
-}
+        private void RandomKaartGenerator(object sender, RoutedEventArgs e)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            int d = 4;
+            int e = 5;
+            int f = 6;
+            int g = 7;
+            int h = 8;
+            int i = 9;
+            int j = 10;
+            int k = 11;
+        }
+
+        private void BtnDeel(object sender, RoutedEventArgs e)
+        {
+            
+
+
+        }
+
+        private void BtnHit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnStand(object sender, RoutedEventArgs e)
+        {
+
+        }
