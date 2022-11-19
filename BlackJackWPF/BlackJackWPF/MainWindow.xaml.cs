4332314536e6b8a869c1b5ca2034e18b80f85d74
Guust Flater <guusflater@gmail.com>
2022-11-19 17:55:39 +0100

    Array en Switch met de waardes verwijderd

--- BlackJackWPF/BlackJackWPF/MainWindow.xaml.cs
+++ BlackJackWPF/BlackJackWPF/MainWindow.xaml.cs
@@ -21,81 +21,10 @@ namespace BlackJackWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        int waarde1 = 1;
-        int waarde2 = 2;
-        int waarde3 = 3;
-        int waarde4 = 4;
-        int waarde5 = 5;
-        int waarde6 = 6;
-        int waarde7 = 7;
-        int waarde8 = 8;
-        int waarde9 = 9;
-        int waarde10 = 10;
-        int boer = 10;
-        int dame = 10;
-        int heer = 10;
 
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Kaarten(string[] args)
-        {
-            string[] Aas = new string[4] { "Klaveren-Aas", "Koeken-Aas", "Harten-Aas", "Schoppen-Aas" };
-            string[] één = new string[4] { "Klaveren-1", "Koeken-1", "Harten-1", "Schoppen-1" };
-            string[] twee = new string[4] { "Klaveren-2", "Koeken-2", "Harten-2", "Schoppen-2" };
-            string[] drie = new string[4] { "Klaveren-3", "Koeken-3", "Harten-3", "Schoppen-3" };
-            string[] vier = new string[4] { "Klaveren-4", "Koeken-4", "Harten-4", "Schoppen-4" };
-            string[] vijf = new string[4] { "Klaveren-5", "Koeken-5", "Harten-5", "Schoppen-5" };
-            string[] zes = new string[4] { "Klaveren-6", "Koeken-6", "Harten-6", "Schoppen-6" };
-            string[] zeven = new string[4] { "Klaveren-7", "Koeken-7", "Harten-7", "Schoppen-7" };
-            string[] acht = new string[4] { "Klaveren-8", "Koeken-8", "Harten-8", "Schoppen-8" };
-            string[] negen = new string[4] { "Klaveren-9", "Koeken-9", "Harten-9", "Schoppen-9" };
-            string[] tien = new string[4] { "Klaveren-10", "Koeken-10", "Harten-10", "Schoppen-10" };
-            string[] boer = new string[4] { "Klaveren-Boer", "Koeken-Boer", "Harten-Boer", "Schoppen-Boer" };
-            string[] dame = new string[4] { "Klaveren-Dame", "Koeken-Dame", "Harten-Dame", "Schoppen-Dame" };
-            string[] heer = new string[4] { "Klaveren-Heer", "Koeken-Heer", "Harten-Heer", "Schoppen-Heer" };
-
-
-
-            Random StringGenerator = new Random();
-            Console.WriteLine(StringGenerator);
-
-
-        }
-
-        private string Geef(string operatie, int opertaie)
-        {
-            switch (operatie)
-            {
-                case "Klaveren-1\", \"Koeken-1\", \"Harten-1\", \"Schoppen-1":
-                    return waarde1;
-                case "\"Klaveren-2\", \"Koeken-2\", \"Harten-2\", \"Schoppen-2\"":
-                    return waarde2;
-                case "\"Klaveren-3\", \"Koeken-3\", \"Harten-3\", \"Schoppen-3\"":
-                    return waarde3;
-                case "\"Klaveren-4\", \"Koeken-4\", \"Harten-4\", \"Schoppen-4\"":
-                    return waarde4;
-                case "\"Klaveren-5\", \"Koeken-5\", \"Harten-5\", \"Schoppen-5\"":
-                    return waarde5;
-                case "\"Klaveren-6\", \"Koeken-6\", \"Harten-6\", \"Schoppen-6\"":
-                    return waarde6;
-                case "\"Klaveren-7\", \"Koeken-7\", \"Harten-7\", \"Schoppen-7\"":
-                    return waarde7;
-                case "\"Klaveren-8\", \"Koeken-8\", \"Harten-8\", \"Schoppen-8\"":
-                    return waarde8;
-                case "\"Klaveren-9\", \"Koeken-9\", \"Harten-9\", \"Schoppen-9\"":
-                    return waarde9;
-                case "\"Klaveren-10\", \"Koeken-10\", \"Harten-10\", \"Schoppen-10\"":
-                    return waarde10
-                case "\"Klaveren-Boer\", \"Koeken-Boer\", \"Harten-Boer\", \"Schoppen-Boer\"":
-                    return boer
-                case "\"Klaveren-Dame\", \"Koeken-Dame\", \"Harten-Dame\", \"Schoppen-Dame\"":
-                    return dame
-                case " \"Klaveren-Heer\", \"Koeken-Heer\", \"Harten-Heer\", \"Schoppen-Heer\"":
-                    return heer
-            }
         }
 
         private void BtnDeel_Click(object sender, RoutedEventArgs e)
